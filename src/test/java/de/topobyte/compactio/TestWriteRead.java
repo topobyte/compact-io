ca926ac29e24d7e8f0bc66406a9b4a11703ba7ea
Sebastian Kürten <sebastian@topobyte.de>
2016-06-16 02:12:02 +0200

    Using diamond operator in one place

--- src/test/java/de/topobyte/compactio/TestWriteRead.java
+++ src/test/java/de/topobyte/compactio/TestWriteRead.java
@@ -33,7 +33,7 @@ public class TestWriteRead
 		int n = 10000;
 
 		Random random = new Random();
-		List<Long> nums = new ArrayList<Long>();
+		List<Long> nums = new ArrayList<>();
 		for (int i = 0; i < n; i++) {
 			long num = random.nextLong();
 			System.out.println(num);
